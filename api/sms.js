6a5d8d9a805e8c40a3ce38eb54c22da6f4ccde41
XI-11-dev <business@elevensolutions.info>
2025-04-16 19:00:40 -0700

    Update sms.js

--- api/sms.js
+++ api/sms.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { from, to, message } = req.body;
 
     if (!from || !to || !message) {
@@ -9,9 +9,9 @@ module.exports = async function handler(req, res) {
     }
 
     try {
-      const response = await axios.post('https://www.didforsale.com/smsapi/send', null, {
+      const response = await axios.post('https://www.didforsale.com/smsapi/sendsms', null, {
         params: {
-          apikey: 'OeEswzA56f648cd8eaafc61bc2d4ed1a91c1a5a87e',  // 
+          apikey: process.env.DFS_APIKEY,
           from,
           to,
           text: message,
@@ -20,7 +20,10 @@ module.exports = async function handler(req, res) {
 
       return res.status(200).json({ success: true, data: response.data });
     } catch (error) {
-      return res.status(500).json({ error: 'Failed to send SMS', details: error.response?.data || error.message });
+      return res.status(500).json({ 
+        error: 'Failed to send SMS', 
+        details: error.response?.data || error.message 
+      });
     }
   } else {
     return res.status(405).json({ error: 'Method not allowed' });
